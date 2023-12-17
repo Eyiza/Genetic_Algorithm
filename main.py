e654d422928aa271f4cb1012edc2cc94977046f1
eyiza <Precious.michael2002@gmail.com>
2023-12-18 00:07:36 +0100

    created function for evaluation phase

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from phases.generate_initial import generateInitialPopulation
+from phases.evaluation import evalation
+
 
 def main():
+    fitness_function = "(4 * x1**2) + (2 * x1 * x2) + (x2 * x3) - (3 * x3**2)" 
+    boundary_range = [(0,4), (2,3), (1,6)] # The boundaries of each string
     num_generations = 100 # Number of generations
     population_size = 100 # Number of chromosomes in the population
     strings = 3 # Number of strings in each chromosome
     gene = 5 # Number of genes/bytes in each string
     chromesome_length = strings * gene # Number of genes/bytes in each chromosome
+
+    best_solution = None # Best solution found so far
+    best_fitness = 0 # Fitness of the best solution found so far
     
+    gen = 0 # Generation number
     population_matrix, mutation_rate, crossover_rate = generateInitialPopulation(population_size, chromesome_length)
 
-    print(population_matrix)
+    parents = evalation(population_matrix, population_size, strings, gene, fitness_function, boundary_range)
+
+    # while gen < num_generations:
+    #     parents = evalation(population_matrix, population_size, strings, gene)
+    #     gen += 1
 
 
 if __name__ == "__main__":
---
+++ phases/evaluation.py
@@ -0,0 +1,35 @@
+"""
+This file contains the evaluation phase of the genetic algorithm.
+"""
+
+import numpy as np
+
+def binary_to_decimal(binary):
+    binary_string = ''.join(map(str, binary)) # Convert each element in the binary list to a string and then join them together.
+    decimal_value = int(binary_string, 2) # Convert the binary string to a decimal value
+    return decimal_value
+
+
+def evalation(population_matrix, population_size, strings, gene, fitness_function, boundary_range):
+    # Decode each string in each chromosome
+    decoded_population = np.zeros((population_size, strings), dtype=int) # Create a matrix to store the decoded population
+    de_values = np.zeros((population_size, strings))
+
+
+    for i in range(population_size): # For each chromosome
+        start_index = 0 # The start index of the current string
+        for j in range(strings): # For each string in the chromosome
+            end_index = start_index + gene # The end index of the current string
+            binary = population_matrix[i, start_index:end_index] # The binary representation of the current string. Note: i is the index of the current row you are interested in
+            decoded_population[i, j] = binary_to_decimal(binary) # The decimal representation of the current string
+            de_values[i, j] = boundary_range[j][0] + ( (decoded_population[i, j] * (boundary_range[j][1] - boundary_range[j][0])) ) / ((2 ** gene) - 1)
+            start_index = end_index # Update the start index for the next string
+
+
+    # Evaluate fitness of each chromosome using the fitness function
+    fitness = np.zeros((population_size, 1)) # Create a matrix to store the fitness of each chromosome
+    for i in range(population_size): 
+        x1, x2, x3 = de_values[i]
+        fitness[i] = eval(fitness_function)
+        
+    return fitness
